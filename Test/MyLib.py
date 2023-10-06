fe9b33279d9242a3bfc1c0f01d046f70214f3b90
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2023-10-06 18:53:50 +0700

    first commit

--- Test/MyLib.py
+++
@@ -1,8 +0,0 @@
-import time
-from typing import Callable
-
-def timeChecking(func: Callable, *args: any, **kwargs: dict) -> float:
-    tic = time.time()
-    func(*args, **kwargs)
-    toc = time.time()
-    return toc - tic
